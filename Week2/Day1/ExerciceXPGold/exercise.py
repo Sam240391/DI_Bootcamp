b0e1baa4a9b52e26db8d05e8288a823996fc9314
Sam240391 <samuelchemla24@gmail.com>
2023-09-03 15:12:56 +0300

    exerciseXP Gold Week 2 day 1

---
+++ Week2/Day1/ExerciceXPGold/exercise.py
@@ -0,0 +1,49 @@
+# Exercise 1 : 
+# Hello World-I Love Python
+# Instructions
+# Print the following output in one line of code:
+# Hello world
+# Hello world
+# Hello world
+# Hello world
+# I love python
+# I love python
+# I love python
+# I love python
+# ------------
+
+# print("Hello world\n" * 4 + "I love python\n" * 4)
+
+# --------------
+
+
+
+# Exercise 2 : What Is The Season ?
+# Instructions
+# Ask the user to input a month (1 to 12).
+# Display the season of the month received :
+# Spring runs from March (3) to May (5)
+# Summer runs from June (6) to August (8)
+# Autumn runs from September (9) to November (11)
+# Winter runs from December (12) to February (2)
+
+# -----------------
+
+user_month = input('In which month we are ? choose a number between 1 and 12!  \n"')
+
+month = int(user_month)
+print(month)
+
+
+if 1 <= month <= 2 or month == 12:
+    print("We are in Winter")
+elif 3 <= month <= 5:
+    print('We are in spring')
+elif 6 <= month <= 8:
+    print('We are in Summer')
+elif 9 <= month <= 11:
+    print('We are in Autumn')
+else:
+    print('You didn"t pick number between 1 and 12')
+
+
