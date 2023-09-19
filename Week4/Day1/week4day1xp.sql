e59d7a6577610a80a6316d451514c727d5cb26a4
Sam240391 <samuelchemla24@gmail.com>
2023-09-19 12:03:06 +0300

    week4 day 1 XP

---
+++ Week4/Day1/week4day1xp.sql
@@ -0,0 +1,41 @@
+-- CREATE TABLE items (
+--     item_id SERIAL PRIMARY KEY,
+--     item_name VARCHAR(255) NOT NULL,
+--     item_price DECIMAL(10, 2) NOT NULL
+-- );
+
+-- INSERT INTO items (item_name, item_price) 
+-- VALUES
+--     ('Small Desk', 100),
+--     ('Large Desk', 300),
+--     ('Fan', 80);
+	
+-- CREATE TABLE customers (
+--     customer_id SERIAL PRIMARY KEY,
+--     first_name VARCHAR(255) NOT NULL,
+--     last_name VARCHAR(255) NOT NULL
+-- );
+
+-- INSERT INTO customers (first_name, last_name) 
+-- VALUES
+--     ('Greg', 'Jones'),
+--     ('Sandra', 'Jones'),
+--     ('Scott', 'Scott'),
+--     ('Trevor', 'Green'),
+--     ('Melanie', 'Johnson');
+
+
+-- SELECT * FROM items;
+
+-- SELECT * FROM items WHERE item_price > 80;
+
+-- SELECT * FROM items WHERE item_price <= 300;
+
+-- SELECT * FROM customers WHERE last_name = 'Smith';
+
+-- SELECT * FROM customers WHERE last_name = 'Jones';
+
+-- SELECT * FROM customers WHERE first_name != 'Scott';
+
+
+
