c07d322e0239914e5cf27c3a42584a5752e9c6e3
Sam240391 <samuelchemla24@gmail.com>
2023-09-27 16:27:48 +0300

    Week 5 Day 1 XP Ninja

---
+++ Week5/Day1/XPNinja.py
@@ -0,0 +1,31 @@
+# def factorial(n):
+#     print(f"Calling factorial({n})")  # This line will help in debugging
+#     if n == 0:  # Base Case
+#         return 1
+#     else:       # Recursive Case
+#         return n * factorial(n - 1)
+    
+
+# print(factorial(5))
+
+# 5 x 4 x 3 x 2
+
+# 5!
+
+# Questions:
+
+# How many times was the factorial function called?
+
+# 6 Times
+
+# Describe the changes in the variable n throughout the execution.
+
+# The variable n is increase to 1 each time that the functiun is calling and when n = 0 we arrive to the base case
+
+# At what point did the function hit the base case?
+
+# when n = 0 we arrive to the base case
+
+# What happens to the call stack when the function hits the base case?
+
+#  When the function hit the base case it's returning to 1 and  so factorial(0) = 1 and n * 1 = 120
