974a5c1c77110bd33f96b5c6234d9e24fcc416ca
Sam240391 <samuelchemla24@gmail.com>
2023-10-31 22:25:32 +0200

    week5 day4 daily challenge Stars

---
+++ Week5/Day4/DAILYCHALLENGE_STARS/script.js
@@ -0,0 +1,16 @@
+
+let pattern = ""
+
+for (let i = 0; i < 6; i++){
+    pattern += "* "
+    console.log(pattern)
+}
+
+
+for (let i = 1; i <= 6; i++) {
+    let row = '';
+    for (let j = 1; j <= i; j++) {
+      row += '* ';
+    }
+    console.log(row);
+}
